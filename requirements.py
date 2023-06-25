d86cae9b60933236e0377d025ecd5ac3090773e9
raut7218 <58078182+raut7218@users.noreply.github.com>
2023-06-25 10:18:59 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,8 +1,8 @@
-pytesseract
-PIL
-gtts
-webbrowser
-datetime
-streamlit
-cv2
-numpy
+pytesseract==5.4.0
+Pillow==9.0.1
+gtts==2.2.2
+webbrowser==1.2
+datetime==4.3
+streamlit==1.12.0
+opencv-python==4.5.5
+numpy==1.22.3
